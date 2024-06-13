19427556c00cf64e530793e5b0af57ae7e65b6bd
andres010adso <andresorlandoguzman54@gmail.com>
2024-06-13 14:26:54 -0500

    este es el ejercicio 1 terminado

---
+++ php.php
@@ -0,0 +1,13 @@
+<?php
+$A = readline("ingrese genero M (masculino) F (femenino)" );
+$B = readline("cuantos años tienes" );
+
+if($a >=  ){
+    echo "eres mayor de edad";
+    $d = readline("eres hombre si o no " );
+    $f = readline("eres mujer si o no " );
+    if($d = si){
+       
+    }
+}
+
